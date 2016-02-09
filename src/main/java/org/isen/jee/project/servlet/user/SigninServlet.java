bddb19b10427b4be3871a49a7d334005edd233fe
Thibault Itart-Longueville <thibault@itart-longueville.fr>
2016-02-09 14:30:20 +0100

    Add User Singin Servlet

---
+++ src/main/java/org/isen/jee/project/servlet/user/SigninServlet.java
@@ -0,0 +1,48 @@
+package org.isen.jee.project.servlet.user;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.persistence.sessions.serializers.JSONSerializer;
+import org.isen.jee.project.dao.UserDao;
+import org.isen.jee.project.model.User;
+
+import com.cedarsoftware.util.io.JsonWriter;
+
+
+@WebServlet("/user/signin")
+public class SigninServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+    	String email = req.getParameter("email");
+    	String password = req.getParameter("password");
+    	if(email == null || password == null || email.isEmpty() || password.isEmpty()){
+    		resp.setStatus(422);
+    		resp.getWriter().print("{ \"error\": \"Missing credentials\" }");
+    		return;
+    	}
+    	UserDao userDao = new UserDao();
+    	User foundUser = userDao.signin(email, password);
+    	if(foundUser != null){
+    		req.getSession().setAttribute("user", foundUser);
+    		String sessionId = req.getSession().getId();
+    		resp.getWriter().print(sessionId);
+    		return;
+    	} else {
+    		resp.setStatus(422);
+    		resp.getWriter().print("{ \"error\": \"Wrong credentials\" }");
+    		return;	
+    	}
+	}
+}

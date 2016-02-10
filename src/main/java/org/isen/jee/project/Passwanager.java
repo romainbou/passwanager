e00bb4093fe7332bc8c6c3aff68187aebff4101c
Thibault Itart-Longueville <thibault@itart-longueville.fr>
2016-02-10 22:24:13 +0100

    Add persistence configuration with mySQL database

---
+++ src/main/java/org/isen/jee/project/Passwanager.java
@@ -0,0 +1,14 @@
+package org.isen.jee.project;
+
+public class Passwanager {
+	
+	Integer id;
+
+    public Integer getId() {
+    	
+    	System.out.println("Hello passwanager");
+    	
+        return id;
+    }
+
+}

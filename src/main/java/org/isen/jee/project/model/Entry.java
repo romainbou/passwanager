40f64f37b32b15b7751a6ff467c4aaa47dddd96f
Rmain Bouyé <romain@bouye.fr>
2016-02-19 22:50:47 +0100

    Update all models ti set Id field as auto generated

--- src/main/java/org/isen/jee/project/model/Entry.java
+++ src/main/java/org/isen/jee/project/model/Entry.java
@@ -23,6 +23,7 @@ public class Entry implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Temporal(TemporalType.TIMESTAMP)

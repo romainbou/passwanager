bab9b155363c7a0da345cf70f6a521e06e9ec24e
Rmain Bouyé <romain@bouye.fr>
2016-02-24 03:15:36 +0100

    Add UserDao class for database interface

---
+++ src/main/java/org/isen/jee/project/UserDao.java
@@ -0,0 +1,55 @@
+package org.isen.jee.project;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+
+public class UserDao {
+	
+//    @Inject
+//    EntityManager em;
+//    @Resource
+//    UserTransaction ut;
+//	
+//    private UserTransaction ut;
+//    @PersistenceContext( unitName = "passwanager" )
+//    private EntityManager em;
+//    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("passwanager");
+//    @PersistenceUnit
+//    EntityManagerFactory emf;
+    EntityManager em = emf.createEntityManager( );
+    
+    public User createNewUser() {
+
+        User user = new User();
+        user.setEmail("romain.bouye@gmail.com");
+        try {
+        	
+//            ut.begin();
+        	em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+//            ut.commit();
+
+//        } catch ( NotSupportedException | SystemException | SecurityException
+//        		| IllegalStateException | RollbackException
+//        		| HeuristicMixedException | HeuristicRollbackException e) {
+//        	return null;
+//        }
+        } catch (SecurityException | IllegalStateException e) {
+            return null;
+        }
+        return user;
+    }
+
+}

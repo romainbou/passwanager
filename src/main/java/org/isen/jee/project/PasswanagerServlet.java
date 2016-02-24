efcef484cf5843d9006d96c5a2160b9158bf7891
Thibault Itart-Longueville <thibault@itart-longueville.fr>
2016-02-24 20:42:56 +0100

    Add main passwanager servlet

---
+++ src/main/java/org/isen/jee/project/PasswanagerServlet.java
@@ -0,0 +1,26 @@
+package org.isen.jee.project;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/passwanager")
+public class PasswanagerServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getWriter().print("Coule");
+		
+	}
+}

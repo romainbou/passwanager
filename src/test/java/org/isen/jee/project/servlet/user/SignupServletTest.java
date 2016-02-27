b72185ca64d658cf0de5334b7bb2ce7171f9cc5c
Rmain Bouyé <romain@bouye.fr>
2016-02-27 15:25:21 +0100

    Add signup test case

---
+++ src/test/java/org/isen/jee/project/servlet/user/SignupServletTest.java
@@ -0,0 +1,44 @@
+package org.isen.jee.project.servlet.user;
+import static org.junit.Assert.assertEquals;
+
+import java.awt.List;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.persistence.oxm.JSONWithPadding;
+import org.isen.jee.project.dao.UserDao;
+import org.isen.jee.project.harness.JettyHarness;
+import org.junit.Test;
+
+public class SignupServletTest extends JettyHarness {
+
+	/**
+     * Dans params, list de tous les users
+     *
+     * GET http://localhost:9090/user
+     *
+     * @throws Exception
+     */
+    @Test
+    public void singupValidUser() throws Exception {
+    	UserDao userDao = new UserDao();
+    	String email = "bar@foo.com";
+    	if(userDao.findByEmail(email) != null){
+    		userDao.delete(email);
+    	}
+    	Map<String, String> params = new HashMap<>();
+    	params.put("email", email);
+    	params.put("firstname", "john");
+    	params.put("lastname", "doe");
+        params.put("username", "john_tester");
+        params.put("password", "hackMe");
+        params.put("public_key", "securePK");
+        assertEquals(200, postAndGetStatusCode(getServletUri(), params));
+    }
+    
+    private String getServletUri() {
+        return getBaseUri() + "/user/signup";
+    }
+
+}

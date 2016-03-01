9c81f43e93c6ed3bf4a7a960c99ff4749d3cac83
Romain Bouyé <romain@bouye.fr>
2016-03-01 14:25:25 +0100

    Add example data to SQL file

--- passwanager.sql
+++ passwanager.sql
@@ -31,6 +31,14 @@ CREATE TABLE `entry` (
   `ref_folder` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `entry`
+--
+
+INSERT INTO `entry` (`id`, `created_at`, `title`, `username`, `url`, `notes`, `ref_user`, `ref_folder`) VALUES
+(9, '2016-03-01 13:44:15', 'Database access', 'admin', 'http://database.com', 'MySQL database', 8, 29),
+(10, '2016-03-01 13:44:49', 'FTP access', 'admin', 'ftp://passwanager.com', '', 8, 29);
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +51,13 @@ CREATE TABLE `folder` (
   `created_at` datetime DEFAULT NULL,
   `ref_owner` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `folder`
+--
+
+INSERT INTO `folder` (`id`, `name`, `created_at`, `ref_owner`) VALUES
+(29, 'Secrets', '2016-03-01 13:41:37', 8);
 
 -- --------------------------------------------------------
 
@@ -60,6 +75,13 @@ CREATE TABLE `user` (
   `public_key` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `firstname`, `lastname`, `username`, `password`, `public_key`) VALUES
+(8, 'john@doe.com', 'John', 'Doe', 'john', '$2a$10$.teQM3gcGBZ/y/2uNEjeZOr2JOer8xgSReDMb.2RXIgnwGMMAXD/W', '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAgjIaqQQ4pryWuL6F/l3xC+phtbEdPg5aU0EDl6o0SZOMX61gzSeVuvvGFNVi\naBpftfGtp0/sge2GM3+hBs2qSqbsDf/Anw+2EiCLOu+7b0+jHpCiWQG6JzP4/oTK4MnvZq1j\nxWM8TpyUVHwQHjLej0MTSZndUFWVAnZEFWGkSLeW6ws1Kl2SdrFbc8KoDKf/sNtzWES4CvFU\nHjs5oSwWOZXYIjvKH8ZUqS5d+i8fj8pke8E4aIGuRb1yiBlYedQopPqgLaH0i3y5Z58+LCA8\noCFSioZUQ9fr6bGSPQLV4k1/XykvjEcsSoMOb7htTrm5mFhyc6lZ49c5voNn+PlxdQIDAQAB\n-----END RSA PUBLIC KEY-----\n\n');
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +92,13 @@ CREATE TABLE `user_folder_link` (
   `user_id` int(11) NOT NULL,
   `folder_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_folder_link`
+--
+
+INSERT INTO `user_folder_link` (`user_id`, `folder_id`) VALUES
+(8, 29);
 
 -- --------------------------------------------------------
 
@@ -135,17 +164,17 @@ ALTER TABLE `value`
 -- AUTO_INCREMENT for table `entry`
 --
 ALTER TABLE `entry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `folder`
 --
 ALTER TABLE `folder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `value`
 --
